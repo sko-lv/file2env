15bdb5bcc76291488c520925511a987a59bc41ca
Oleh Sko <notaruta@domain,com>
2018-08-11 17:42:04 +0300

    Initial commit.Tested OK

---
+++ README.md
@@ -0,0 +1,9 @@
+#file2env  
+Exports ENV variables from vars-file.txt (default)   
+###Run: ./file2env.sh vars2.txt  
+### vars-file.txt format:  
+...  
+ VAR=varsring  
+...  
+   ./file2env.sh vars2.txt  
+
---
+++ file2env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set +x
+###################################
+## Read lines from properties file 
+#  and export them into env vars
+#
+#~~ 
+#  Usage: $0 [./vars-file.txt]
+#~
+
+VARSFILE=${PWD}/vars-file.txt
+
+export_lines() {
+    #echo ==$1
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        [ ! -z $(echo "$line"|grep -Ev '^#' 2>/dev/null) ] && export "$line"
+    done < "$1"
+}
+
+## Main 
+if ( [ ! -f "$VARSFILE" ] && [ ! -f "$1" ] ) ; then
+    sed -ni '/#~~/,/#~/p' $0
+  else
+     [ -s "$VARSFILE" ] && export_lines "$VARSFILE"
+     [ -s "$1" ] && export_lines "$1"
+ 
+fi
+env |grep '^VAR'
