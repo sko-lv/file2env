e081e5dc4cef3f5d408dd444cc4e9252cac4aef0
Oleh Skoropad <Oleh_Skoropad@epam.com>
2018-08-15 13:03:29 +0300

    Added help info. Fixed bugs.

--- file2env.sh
+++ file2env.sh
@@ -3,9 +3,21 @@ set +x
 ###################################
 ## Read lines from properties file 
 #  and export them into env vars
-#
+###################################
+
 #~  ~ 
-#  Usage: $0 [./vars-file.txt]
+#
+# Usage: file2env.sh [./vars-file.txt]
+#
+#  == Format of vars-file.txt ==
+#  .....
+#  #This is comment line
+#  VAR1=varstring1
+#  VAR2=varstring2
+#  #Next line is to unset VAR
+#  -n VAR3
+#  .....
+#
 #~ ~~
 
 VARSFILE=${PWD}/vars-file.txt
@@ -19,10 +31,12 @@ export_lines() {
 
 ## Main 
 if ( [ ! -f "$VARSFILE" ] && [ ! -f "$1" ] ) ; then
-     cat $0 | sed -n '/^#~  ~/,/^#~ ~~/p;/^#~  ~~/q'
+     echo ERROR: no VARSFILE found! Exitting ...
+     cat $0| sed -n '/^#~  ~/,/^#~ ~~/p;/^#~  ~~/q'| sed 's/^#//'| grep -v '^~'
   else
      [ -s "$VARSFILE" ] && export_lines "$VARSFILE"
+     [ ! -f "$1" ] && echo WARNING: no such file $1. Exporting from default file.
      [ -s "$1" ] && export_lines "$1"
  
 fi
-env |grep '^VAR'
+
